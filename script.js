8da4d7bb1abe5609a05e9b3aaf2b7f41691d3e33
Eduarda Monteblanco <48218233+Eduarda-Monteblanco@users.noreply.github.com>
2022-08-24 10:45:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const moves = document.getElementById("moves-count");
-const timeValue = document.getElementById('time');
+const timeValue = document.getElementById("time");
 const startButton = document.getElementById("start");
 const stopButton = document.getElementById('stop');
 const gameContainer = document.querySelector(".game-container");
@@ -28,7 +28,7 @@ const items = [
     { name: "toucan", image: "./images/toucan.png" },
   ];
 
-  let second = 0, minutes = 0;
+let seconds = 0, minutes = 0;
 
 let movesCount = 0, winCount = 0;
 
@@ -42,9 +42,9 @@ const timeGenerator = () => {
 
     let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
 
-    let minutesValue = minnutes < 10 ? `0${minutes}` : minutes;
+    let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
 
-    timeValue.innerHTML = `<span>Time:<span> ${minutesValue}:${secondsValue}`; 
+    timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`; 
 }
 
 const movesCounter = () => {
@@ -78,7 +78,71 @@ const matrixGenerator = (cardValues, size = 4) => {
     }
 
     gameContainer.style.gridTemplateColumns = `repeat(${size}, auto)`
+
+    cards =document.querySelectorAll(".card-container");
+    cards.forEach((card) => {
+        card.addEventListener("click", () => {
+            if(!card.classList.contains("matched")) {
+                card.classList.add("flipped");
+            if(!firstCard) {
+                firstCard = card;
+                firstCardValue = card.getAttribute("data-card-value")
+            } 
+            else {
+                movesCounter();
+                secondCard = card;
+                let secondCardValue = card.getAttribute("data-card-value")
+                if(firstCardValue == secondCardValue) {
+                    firstCard.classList.add("matched")
+                    secondCard.classList.add("matched")
+                    firstCard = false;
+
+                    winCount += 1;
+
+                    if(winCount == Math.floor(cardValues.length / 2)) {
+                        result.innerHTML = `<h2> Você venceu </h2>
+                        <h4>Movimentos: ${movesCount}</h4>` 
+                        stopGame()
+                    } 
+                } else {
+                    let [tempFirst, tempSecond] = [firstCard, secondCard]
+                    firstCard = false;
+                    secondCard = false
+
+                    let delay = setTimeout( () => {
+                        tempFirst.classList.remove("flipped")
+                        tempSecond.classList.remove("flipped")
+                    }, 900)
+                }
+            }
+        }
+           
+        })
+    })
 }
+
+startButton.addEventListener("click", () => {
+    movesCount = 0;
+    seconds = 0;
+    minutes = 0;
+
+    controls.classList.add("hide"); 
+    stopButton.classList.remove("hide")
+    startButton.classList.add("hide")
+
+    interval = setInterval(timeGenerator, 1000)
+
+    moves.innerHTML = `<span>Moves:</span> ${movesCount}`
+
+    initializer()
+})
+
+stopButton.addEventListener("click", (stopGame = () => {
+    controls.classList.remove("hide")
+    stopButton.classList.add("hide")
+    startButton.classList.remove("hide")
+    clearInterval(interval)
+}))
 
 const initializer = () => {
     result.innerText = "";
@@ -88,4 +152,3 @@ const initializer = () => {
     matrixGenerator(cardValues);
 }
 
-initializer();
